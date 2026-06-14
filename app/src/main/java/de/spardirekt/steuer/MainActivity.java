4b697d87daba4a69b9012c5ac5bf42812bc988ff
gangster2490 <robertlind41@googlemail.com>
2026-06-14 22:22:49 +0200

    Fix: remove BuildConfig, clean imports

--- app/src/main/java/de/spardirekt/steuer/MainActivity.java
+++ app/src/main/java/de/spardirekt/steuer/MainActivity.java
@@ -6,76 +6,77 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.webkit.*;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.core.content.FileProvider;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
     private WebView webView;
     private ValueCallback<Uri[]> filePathCallback;
     private Uri cameraImageUri;
     private static final int FILE_CHOOSER_REQUEST = 1;
+    private static final String FILE_PROVIDER = "de.spardirekt.steuer.fileprovider";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
 
         webView = new WebView(this);
-        webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         setContentView(webView);
 
-        WebSettings s = webView.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setAllowContentAccess(true);
-        s.setMediaPlaybackRequiresUserGesture(false);
-        s.setCacheMode(WebSettings.LOAD_DEFAULT);
-        s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
-
+        webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public boolean onShowFileChooser(WebView wv, ValueCallback<Uri[]> cb,
+            public boolean onShowFileChooser(WebView wv, ValueCallback<Uri[]> callback,
                     FileChooserParams params) {
-                if (filePathCallback != null) filePathCallback.onReceiveValue(null);
-                filePathCallback = cb;
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+                filePathCallback = callback;
 
-                String[] accept = params.getAcceptTypes();
                 boolean capture = params.isCaptureEnabled();
-
                 if (capture) {
                     try {
                         File imgFile = createImageFile();
-                        cameraImageUri = FileProvider.getUriForFile(MainActivity.this,
-                                BuildConfig.APPLICATION_ID + ".fileprovider", imgFile);
-                        Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        cam.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
-                        startActivityForResult(cam, FILE_CHOOSER_REQUEST);
+                        cameraImageUri = FileProvider.getUriForFile(
+                            MainActivity.this, FILE_PROVIDER, imgFile);
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+                        startActivityForResult(intent, FILE_CHOOSER_REQUEST);
                     } catch (IOException e) {
                         filePathCallback = null;
                     }
                 } else {
-                    Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
-                    gallery.setType("image/*");
-                    startActivityForResult(Intent.createChooser(gallery, "Bild wählen"),
-                            FILE_CHOOSER_REQUEST);
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                        Intent.createChooser(intent, "Bild waehlen"),
+                        FILE_CHOOSER_REQUEST
+                    );
                 }
                 return true;
             }
@@ -85,16 +86,16 @@ public class MainActivity extends Activity {
     }
 
     private File createImageFile() throws IOException {
-        String stamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String stamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
         File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         return File.createTempFile("BELEG_" + stamp, ".jpg", dir);
     }
 
     @Override
-    protected void onActivityResult(int req, int res, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (filePathCallback == null) return;
         Uri[] results = null;
-        if (res == RESULT_OK) {
+        if (resultCode == RESULT_OK) {
             if (data != null && data.getData() != null) {
                 results = new Uri[]{data.getData()};
             } else if (cameraImageUri != null) {
@@ -108,7 +109,10 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
